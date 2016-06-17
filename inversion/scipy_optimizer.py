7f367c40489c1736f2145efb9315b155d686d9d9
Leonardo Uieda <leouieda@gmail.com>
2016-06-17 20:36:16 -0300

    Scipy optimizer checks if needs jac and hess

--- inversion/scipy_optimizer.py
+++ inversion/scipy_optimizer.py
@@ -7,6 +7,9 @@ from scipy import optimize
 
 class ScipyOptimizer(object):
 
+    needs_jac = 'CG BFGS Newton-CG L-BFGS-B TNC SLSQP dogleg trust-ncg'.split()
+    needs_hess = 'Newton-CG dogleg trust-ncg'.split()
+
     def __init__(self, method, **kwargs):
         self.method = self._is_valid(method)
         self.kwargs = kwargs
@@ -15,11 +18,13 @@ class ScipyOptimizer(object):
         return method
 
     def minimize(self, objective):
-        res = optimize.minimize(fun=objective.value,
-                                method=self.method,
-                                jac=objective.gradient,
-                                hess=objective.hessian,
-                                **self.kwargs)
+        args = dict(self.kwargs)
+        if self.method in self.needs_jac:
+            args['jac'] = objective.gradient
+        if self.method in self.needs_hess:
+            args['hess'] = objective.hessian
+        res = optimize.minimize(fun=objective.value, method=self.method,
+                                **args)
         self.stats = res
         return res.x
 
