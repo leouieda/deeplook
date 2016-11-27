a4aa13e91db16eba702a4ec671b34b730ab2b740
Leonardo Uieda <leouieda@gmail.com>
2016-11-27 14:32:00 -0200

    Remove inner inversion package (added by mistake)
    Move test running function to tests/run_tests.py

--- deeplook/__init__.py
+++ deeplook/__init__.py
@@ -6,41 +6,7 @@ from .linear_solver import LinearOptimizer
 from .gradient_descent import Newton, LevMarq, SteepestDescent
 from .heuristic import ACOR
 from .scipy_optimizer import ScipyOptimizer
+from .tests.run_tests import test
 
 
 __version__ = 0.1
-
-
-def test(doctest=True, verbose=False, coverage=False):
-    """
-    Run the test suite.
-
-    Uses `py.test <http://pytest.org/>`__ to discover and run the tests.
-
-    Parameters:
-
-    * doctest : bool
-        If ``True``, will run the doctests as well (code examples that start
-        with a ``>>>`` in the docs).
-    * verbose : bool
-        If ``True``, will print extra information during the test run.
-    * coverage : bool
-        If ``True``, will run test coverage analysis on the code as well.
-        Requires ``pytest-cov``.
-
-    Returns:
-
-    * exit_code : int
-        The exit code for the test run. If ``0``, then all tests pass.
-
-    """
-    import pytest
-    args = []
-    if verbose:
-        args.append('-v')
-    if coverage:
-        args.append('--cov-report term-missing --cov=deeplook')
-    if doctest:
-        args.append('--doctest-modules')
-    args.append('--pyargs deeplook')
-    return pytest.main(' '.join(args))
--- deeplook/inversion/__init__.py
+++
@@ -1,8 +0,0 @@
-from .misfit import L2Norm, L2NormLinear
-from .models import NonLinearModel, LinearModel
-from .regularization import Damping, Smoothness, Smoothness1D, \
-                            TotalVariation, TotalVariation1D
-from .linear_solver import LinearOptimizer
-from .gradient_descent import Newton, LevMarq, SteepestDescent
-from .heuristic import ACOR
-from .scipy_optimizer import ScipyOptimizer
